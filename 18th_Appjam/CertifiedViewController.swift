e2db23a531571b18b515d1203256f0b088b0df41
PanBle <ktm0921978@gmail.com>
2019-04-20 20:52:12 +0900

    add pod, login view

---
+++ 18th_Appjam/CertifiedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CertifiedViewController.swift
+//  18th_Appjam
+//
+//  Created by KM_TM on 20/04/2019.
+//  Copyright © 2019 KM_TM. All rights reserved.
+//
+
+import UIKit
+
+class CertifiedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@
 pod 'Alamofire', '~> 5.0.0-beta.5'
 pod 'SwiftyJSON', '~> 4.0'
 pod 'Toast-Swift', '~> 5.0.0'
+pod 'SnapKit', '~> 4.0.0'
 
 target '18th_Appjam' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
