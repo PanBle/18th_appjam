2d2dd9e35cf50e7cd664a03a1ac04322c502c984
PanBle <ktm0921978@gmail.com>
2019-04-21 03:54:13 +0900

    register add

--- 18th_Appjam/Register1ViewController.swift
+++ 18th_Appjam/Register1ViewController.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class Register1ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwdLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwd1TextField: UITextField!
+    @IBOutlet weak var passwd2TextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.emailLabel.text = ""
+        self.passwdLabel.text = ""
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func CompleteButton(_ sender: Any) {
+        if validateEmail() && passwd1TextField.text! == passwd2TextField.text!
+        {
+            performSegue(withIdentifier: "Register2Segue", sender: nil)
+        }
+        else{
+            self.emailLabel.text = "이메일을 확인해주세요!"
+            self.emailLabel.text = "비밀번호를 확인해주세요!"
+        }
+        
     }
-    */
-
+    func validateEmail() -> Bool {
+        let emailRegEx = "^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$"
+        let predicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return predicate.evaluate(with: self)
+    }
+    @IBAction func PopButton(_ sender: Any) {
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- 18th_Appjam/Register2ViewController.swift
+++ 18th_Appjam/Register2ViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class Register2ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imageviewcircler()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func imageviewcircler() {
+        self.imageView.layer.cornerRadius = self.imageView.frame.size.width / 2
+        self.imageView.clipsToBounds = true
     }
-    */
-
+    
+    @IBAction func completeButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "", sender: nil)
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
